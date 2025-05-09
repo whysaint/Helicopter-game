862145bda52c6ec78bf32771a938658bc1fb74ad
whysaint <taras.panchuk.un@gmail.com>
2025-05-09 12:13:46 +0300

    Город

---
+++ Assets/scripts/CameraFolow.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFolow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 camera = new Vector3(0f, 1f, -10f);
+    public float speed;
+    
+    void Update()
+    {
+        Vector3 FactOffset = target.position + camera;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, FactOffset, speed * Time.deltaTime);
+
+        transform.position = smoothedPosition;
+        transform.LookAt(target.position + Vector3.forward * 10f);
+    }
+}
