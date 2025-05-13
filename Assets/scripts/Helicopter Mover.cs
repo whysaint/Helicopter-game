b47a0961ed9c1d6825a44c9bfc2eb39a79676a52
WhySaint <taras.panchuk.un@gmail.com>
2025-05-13 22:27:15 +0300

    Game For Android

--- Assets/scripts/Helicopter Mover.cs
+++ Assets/scripts/Helicopter Mover.cs
@@ -6,6 +6,9 @@ public class HelicopterMover : MonoBehaviour
     public float SpeedRotate;
     public float StabilityForce = 3.5f;
     private Rigidbody _rb;
+    
+    private float _verticalInput = 0f;
+    private float _horizontalInput = 0f;
 
     private void Start()
     {
@@ -14,8 +17,14 @@ public class HelicopterMover : MonoBehaviour
 
     void FixedUpdate()
     {
-        _rb.AddRelativeForce(0f, Input.GetAxis("Vertical") * SpeedMove, 0f);
-        _rb.AddTorque(0f, 0f, -Input.GetAxis("Horizontal") * SpeedRotate );
+        float vertical = Input.GetAxis("Vertical") + _verticalInput;
+        float horizontal = Input.GetAxis("Horizontal") + _horizontalInput;
+
+        vertical = Mathf.Clamp(vertical, -1f, 1f);
+        horizontal = Mathf.Clamp(horizontal, -1f, 1f);
+
+        _rb.AddRelativeForce(0f, vertical * SpeedMove, 0f);
+        _rb.AddTorque(0f, 0f, -horizontal * SpeedRotate);
         Stabilize();
     }
 
@@ -25,7 +34,26 @@ public class HelicopterMover : MonoBehaviour
         Vector3 factUp = Vector3.up;
         Vector3 finalUp = Vector3.Cross(currutUp, factUp);
         _rb.AddTorque(finalUp * StabilityForce);
-        
+    }
+    
+    public void SetVerticalInput(float value)
+    {
+        _verticalInput = value;
+    }
+
+    public void SetHorizontalInput(float value)
+    {
+        _horizontalInput = value;
+    }
+
+    public void ResetVerticalInput()
+    {
+        _verticalInput = 0f;
+    }
+
+    public void ResetHorizontalInput()
+    {
+        _horizontalInput = 0f;
     }
 
 }
---
+++ Assets/testBthUpClick.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testBthUpClick : MonoBehaviour
+{
+    public void TestClick()
+    {
+        Debug.Log("click peiwthbguj7887787878789");
+    }
+
+}
