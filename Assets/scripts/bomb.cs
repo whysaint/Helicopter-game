4ea38e5692a6bcec52dfdc82d4fb7e9200cd9f6c
deicide <taraspanchuk@MacBookAir.la.net.ua>
2025-05-21 23:07:20 +0300

    BOOM efect

--- Assets/scripts/bomb.cs
+++ Assets/scripts/bomb.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,19 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     public AudioSource bombSound;
+    public ParticleSystem explosionEffect;
+    public float timer;
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Bomb"))
         {
+            ParticleSystem effect = Instantiate(explosionEffect, collision.transform.position, Quaternion.identity);
+            effect.Play();
+            float totalDuration = effect.main.duration + effect.main.startLifetime.constantMax;
+            Destroy(effect.gameObject, totalDuration);
             bombSound.Play();
+            Destroy(collision.gameObject);
         }
     }
 }
