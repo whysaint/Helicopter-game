996fb0abbdb43699d9d7251c82de9c787440ad7c
whysaint <taras.panchuk.un@gmail.com>
2026-01-14 19:17:57 +0100

    optimized game + refactor bomb script"

--- Assets/scripts/bomb.cs
+++ Assets/scripts/bomb.cs
@@ -3,22 +3,15 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     [SerializeField] public ParticleSystem explosionEffect;
-    [SerializeField] public float bounceForce;
     
-    public float timer;
-
-
-    void OnTriggerEnter(Collider other)
+    void OnCollisionEnter(Collision other)
     {
         if (other.transform.root.CompareTag("Player"))
         {
             ParticleSystem effect = Instantiate(explosionEffect, transform.position, Quaternion.identity);
-            
             effect.Play();
             
-            float totalDuration = effect.main.duration + effect.main.startLifetime.constantMax;
-            
-            Destroy(effect.gameObject, totalDuration);
+            Destroy(effect.gameObject, effect.main.duration);
             
             AudioManader.Instance.PlaySound(Soundtype.BombExplosion);
             Destroy(gameObject);
