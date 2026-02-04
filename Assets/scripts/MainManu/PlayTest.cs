6748c2971166ad2ad8575331d6301d47645c3d61
whysaint <taras.panchuk.un@gmail.com>
2026-02-04 19:00:16 +0100

    new build profile + load screan

--- Assets/scripts/MainManu/PlayTest.cs
+++ Assets/scripts/MainManu/PlayTest.cs
@@ -3,8 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class PlayTest : MonoBehaviour
 {
+    [SerializeField] private GameObject g;
+    
     public void OnClick()
     {
+        g.SetActive(true);
         SceneManager.LoadScene("1");
     }
+    
 }
